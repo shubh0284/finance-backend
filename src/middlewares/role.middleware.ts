8d7bffb1c1b7b16fd6951a5d36316d666452b9f3
shubhamshinare <shubhamshinare2004@gmail.com>
2026-04-02 20:08:09 +0530

    Added authentication and role-based access control

---
+++ src/middlewares/role.middleware.ts
@@ -0,0 +1,14 @@
+import { Response, NextFunction } from "express";
+import { AuthRequest } from "./auth.middleware";
+
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: AuthRequest, res: Response, next: NextFunction) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: "Access denied",
+      });
+    }
+
+    next();
+  };
+};
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,31 @@
 import express from "express";
 import { register, login } from "../controllers/auth.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { authorizeRoles } from "../middlewares/role.middleware";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+
+//protected route(only admin)
+router.get("/admin", authMiddleware, authorizeRoles("admin"), (req, res) => {
+  res.json({ message: "Welcome Admin" });
+});
+
+//analyst and admin
+router.get(
+  "/analytics",
+  authMiddleware,
+  authorizeRoles("admin", "analyst"),
+  (req, res) => {
+    res.json({ message: "Analytics data" });
+  },
+);
+
+//all logged-in user
+router.get("/profile", authMiddleware, (req, res) => {
+  res.json({ message: "User profile" });
+});
 
 export default router;
